0de94224d923f768aa5e011c3549057547424958
gg17117144 <gg17117144@gmail.com>
2025-08-04 21:29:26 +0800

    feat: 更新事件中心實作

--- assets/slotBar/scripts/eventSystem/EventCenter.ts
+++ assets/slotBar/scripts/eventSystem/EventCenter.ts
@@ -1,4 +1,14 @@
 import { EventTarget } from 'cc';
 
+// 拉霸的事件
 const slotBarEventBus = new EventTarget();
-export default slotBarEventBus;
+// 登入介面的事件
+const loginViewEventBus = new EventTarget();
+// 登入介面的事件
+const tipTextEventBus = new EventTarget();
+
+export default {
+    slotBarEventBus,
+    loginViewEventBus,
+    tipTextEventBus,
+};
--- assets/slotBar/scripts/eventSystem/EventTypes.ts
+++ assets/slotBar/scripts/eventSystem/EventTypes.ts
@@ -6,4 +6,14 @@ export enum soltEventTypes {
     AllReelsFinished = 'AllReelsFinished', // 所有滾輪完成旋轉
     ShowResult = 'ShowResult', // 顯示結果
     Jackpot = 'Jackpot', // 中大獎(之後在加畫面)
+    UpdateCoinText = 'updateCoinText', // 中大獎(之後在加畫面)
+}
+
+export enum loginViewEventTypes {
+    OpenLoginView = 'OpenLoginView', // 開啟登入介面
+    OpenRegisterView = 'OpenRegisterView',// 開啟註冊介面
+}
+
+export enum tipTextEventTypes {
+    ShowTipText = 'ShowTipText', // 開啟登入介面
 }
